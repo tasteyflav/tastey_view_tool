3df8dbd0bb32ba2796cb330517c7debfc045680f
Mark Venema <markavenema@gmail.com>
2018-04-15 08:15:30 +0900

    Dev tool intitial commit

---
+++ lib/tastey_view_tool.rb
@@ -0,0 +1,6 @@
+require "tastey_view_tool/version"
+require "tastey_view_tool/renderer"
+
+module TasteyViewTool
+  # Your code goes here...
+end
---
+++ lib/tastey_view_tool/version.rb
@@ -0,0 +1,3 @@
+module TasteyViewTool
+  VERSION = "0.1.0"
+end
